888891d68dc46b9a5c8dd941b321fa0b6b65ea7a
Alva Chien <alvachien@163.com>
2015-11-11 15:50:38 +0800

    Building the applications.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # acNotes
-Notes built by Node.js;
+Notes built by Node.js/Express.js;
+
+Histories:
+1. Application initialized.
+
+Credits:
--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 app.use('/', routes);
-app.use('/users', users);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
